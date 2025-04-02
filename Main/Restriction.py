e5645b42d276047fb3db8fea48db1b807783e70d
PJayJay <pfield@ithaca.edu>
2025-04-01 23:34:41 -0400

    Created classes for demo

---
+++ Main/Restriction.py
@@ -0,0 +1,6 @@
+class Restriction:
+    def __init__(self,isVegan,isVegeterian,allergies):
+        this.isVegan=isVegan
+        this.isVegetarian=isVegetarian
+        this.allergies=allergies
+        
---
+++ Main/User.py
@@ -0,0 +1,11 @@
+class User:
+
+    def __init__(self,height,weight):
+        self.height=height
+        self.weight=weight
+        avail=0
+        limits=null
+        goals=null
+
+    
+    
